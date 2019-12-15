90fa2cf514f714cf73eb162fab87e0d0f881faff
Geguyue-ml <1021170632@qq.com>
2019-12-16 00:20:09 +0800

    任务列表完成

--- lin-lang/src/components/TaskList.vue
+++ lin-lang/src/components/TaskList.vue
@@ -38,12 +38,12 @@
                 <table>
                     <thead>
                         <tr>
-                            <td style="width:320px;">商品信息</td>
-                            <td>任务信息</td>
+                            <td style="width:360px;">商品信息</td>
+                            <td style="width: 250px;">任务信息</td>
                             <td>数量信息</td>
                             <td>代办事项</td>
                             <td>任务状态</td>
-                            <td>操作</td>
+                            <td style="width: 140px;">操作</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -62,57 +62,82 @@
                                 </div>
                             </td>
                             <td>
-                                <p>任务类型：折扣试用</p>
-                                <p>任务编号：lsaidfjwlefjwoief1233</p>
-                                <p>投放时间：2019-12-12</p>
+                                <p class="tdLine">任务类型：折扣试用</p>
+                                <p class="tdLine">任务编号：lsaidfjwlefjwoief1233</p>
+                                <p class="tdLine">投放时间：2019-12-12</p>
                             </td>
                             <td>
-                                <p>投放数量：20</p>
-                                <p>申请人数：0</p>
-                                <p>已下单：0</p>
-                                <p>已完成：0</p>
-                                <p>未完成：0</p>
+                                <p class="tdLine">投放数量：20</p>
+                                <p class="tdLine">申请人数：0</p>
+                                <p class="tdLine">已下单：0</p>
+                                <p class="tdLine">已完成：0</p>
+                                <p class="tdLine">未完成：0</p>
                             </td>
                             <td>
-                                <p>待审核订单金额：0</p>
-                                <p>待审核好评：0</p>
-                                <p>待审核好评截图：0</p>
+                                <p class="tdLine">待审核订单金额：0</p>
+                                <p class="tdLine">待审核好评：0</p>
+                                <p class="tdLine">待审核好评截图：0</p>
                             </td>
-                            <td>
+                            <td class="tdCenter">
                                 <el-tag>标签一</el-tag>
                             </td>
+                            <td class="tdCenter">
+                                <el-select v-model="operation" placeholder="操作" size="mini">
+                                    <el-option label="继续投放" value="0"></el-option>
+                                    <el-option label="删除任务" value="1"></el-option>
+                                    <el-option label="投放加速" value="2"></el-option>
+                                    <el-option label="立即付款" value="3"></el-option>
+                                </el-select>
+                            </td>
+                        </tr>
+                        <tr>
                             <td>
-                                <el-popover
-                                    placement="bottom"
-                                    title="标题"
-                                    width="200"
-                                    trigger="click"
-                                    content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
-                                    <el-button slot="reference"><i class="el-icon-s-grid"></i>操作</el-button>
-                                </el-popover>
+                                <div class="iBlock imgFrm">
+                                    <img src="../assets/goodsImg.jpg">
+                                </div>
+                                <div class="iBlock">
+                                    <div class="shopName">
+                                        <img src="../assets/tb_small.png">
+                                        <span >MIKU521</span>
+                                    </div>
+                                    <p class="shopTitle">商品标题1介绍及详情等等商品标题1介绍及详情等等商品标题1介绍及详情等等</p>
+                                    <span class="shopPrice">23元</span>
+                                </div>
+                            </td>
+                            <td>
+                                <p class="tdLine">任务类型：折扣试用</p>
+                                <p class="tdLine">任务编号：lsaidfjwlefjwoief1233</p>
+                                <p class="tdLine">投放时间：2019-12-12</p>
+                            </td>
+                            <td>
+                                <p class="tdLine">投放数量：20</p>
+                                <p class="tdLine">申请人数：0</p>
+                                <p class="tdLine">已下单：0</p>
+                                <p class="tdLine">已完成：0</p>
+                                <p class="tdLine">未完成：0</p>
+                            </td>
+                            <td>
+                                <p class="tdLine">待审核订单金额：0</p>
+                                <p class="tdLine">待审核好评：0</p>
+                                <p class="tdLine">待审核好评截图：0</p>
+                            </td>
+                            <td class="tdCenter">
+                                <el-tag>标签一</el-tag>
+                            </td>
+                            <td class="tdCenter">
+                                <el-select v-model="operation" placeholder="操作" size="mini">
+                                    <el-option label="继续投放" value="0"></el-option>
+                                    <el-option label="删除任务" value="1"></el-option>
+                                    <el-option label="投放加速" value="2"></el-option>
+                                    <el-option label="立即付款" value="3"></el-option>
+                                </el-select>
                             </td>
                         </tr>
                     </tbody>
                 </table>
-                <!-- <el-table :data="tableData" style="width: 100%">
-                    <el-table-column fixed prop="orderImg" label="商品主图" width="150"></el-table-column>
-                    <el-table-column prop="shop" label="投放店铺" width="120"></el-table-column>
-                    <el-table-column prop="price" label="商品价格" width="120"></el-table-column>
-                    <el-table-column prop="pushNum" label="投放数量" width="120"></el-table-column>
-                    <el-table-column prop="applyPerson" label="申请人数" width="300"></el-table-column>
-                    <el-table-column prop="pushOrder" label="已下单" width="120"></el-table-column>
-                    <el-table-column prop="success" label="已完成" width="120"></el-table-column>
-                    <el-table-column prop="unSuccess" label="未完成" width="120"></el-table-column>
-                    <el-table-column prop="orderAmount" label="待审核订单金额" width="120"></el-table-column>
-                    <el-table-column prop="orderGood" label="待审核好评" width="120"></el-table-column>
-                    <el-table-column prop="orderImg" label="待审核好评截图" width="120"></el-table-column>
-                    <el-table-column prop="orderState" label="状态" width="120"></el-table-column>
-                    <el-table-column fixed="right" label="操作" width="120">
-                        <template slot-scope="scope">
-                            <el-button @click.native.prevent="sendTask(scope.$index, tableData)" type="text" size="small"> 发布任务 </el-button>
-                        </template>
-                    </el-table-column>
-                </el-table> -->
+            </div>
+            <div class="pageInation">
+                <el-pagination background layout="prev, pager, next" :total="1000" :page-size="50" @current-change="handleCurrentChange"></el-pagination>
             </div>
         </div>
     </div>
@@ -130,15 +155,8 @@ export default {
         storeList: ['所有店铺', '店铺B', '店铺C', '店铺D', '店铺E', '店铺F', '店铺G', '店铺H', '店铺I'],
         taskType: '0',
         filterInfo: '',
-        tableData: [
-            {orderImg: '#', shop: '淘宝', price: '12', pushNum: '12', applyPerson: '2', pushOrder: 1, success: 12, unSuccess: 2, orderAmount: '21.6', orderGood: 2, orderImg: 1, orderState: 1}, 
-            {orderImg: '#', shop: '京东', price: '23', pushNum: '23', applyPerson: '4', pushOrder: 1, success: 7, unSuccess: 4, orderAmount: '21.6', orderGood: 2, orderImg: 1, orderState: 1}, 
-            {orderImg: '#', shop: '天猫', price: '65', pushNum: '34', applyPerson: '5', pushOrder: 22, success: 9, unSuccess: 2, orderAmount: '21.6', orderGood: 2, orderImg: 1, orderState: 1}, 
-            {orderImg: '#', shop: '天猫', price: '45', pushNum: '43', applyPerson: '8', pushOrder: 13, success: 14, unSuccess: 6, orderAmount: '21.6', orderGood: 2, orderImg: 1, orderState: 1}, 
-            {orderImg: '#', shop: '天猫', price: '132', pushNum: '45', applyPerson: '1', pushOrder: 7, success: 13, unSuccess: 2, orderAmount: '21.6', orderGood: 2, orderImg: 1, orderState: 1}, 
-            {orderImg: '#', shop: '京东', price: '4586', pushNum: '6', applyPerson: '5', pushOrder: 3, success: 4, unSuccess: 1, orderAmount: '21.6', orderGood: 2, orderImg: 1, orderState: 1}, 
-            {orderImg: '#', shop: '淘宝', price: '5', pushNum: '19', applyPerson: '7', pushOrder: 8, success: 2, unSuccess: 0, orderAmount: '21.6', orderGood: 2, orderImg: 1, orderState: 1}
-        ]
+        tableData: [],
+        operation: '1',
     }
   },
   methods: {
@@ -147,6 +165,9 @@ export default {
       },
       sendTask(index, rows) {
         rows.splice(index, 1);
+      },
+      handleCurrentChange(val){
+          console.log("当前给后台的页数：" + val);
       }
   }
 }
@@ -170,7 +191,7 @@ export default {
     padding: 25px 0;
     text-align: left;
 }
-.el-select{
+.filterFrm .el-select{
     margin-right: 25px;
 }
 .filterItem{
@@ -187,16 +208,25 @@ export default {
     border-collapse: collapse;
 }
 .tableFrm td{
-    padding: 15px 0;
+    padding: 15px;
 }
 .tableFrm thead td{
     font-size: 15px;
     font-weight: bold;
-    color: #999;
+    color: var(--on-color);
     text-align: center;
 }
-.tableFrm tbody td{
-    border-bottom: 1px solid #e6e6e6;
+.tableFrm td{
+    border: 1px solid #e6e6e6;
+}
+.tdLine{
+    padding: 10px;
+}
+.tdCenter{
+    text-align: center;
+}
+.tdCenter .el-select{
+    width: 100%;
 }
 .iBlock{
     display: inline-block;
@@ -215,7 +245,7 @@ export default {
     vertical-align: middle;
 }
 .shopTitle{
-    max-width: 200px;
+    max-width: 215px;
     text-overflow: -o-ellipsis-lastline;
     overflow: hidden;
     text-overflow: ellipsis;
@@ -223,12 +253,15 @@ export default {
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
     margin-bottom: 15px;
-    color: #000;
     font-size: 15px;
 }
 .shopPrice{
     color: var(--on-color);
     font-size: 16px;
     font-weight: bold;
+}
+.pageInation{
+    text-align: right;
+    margin-top: 25px;
 }
 </style>
